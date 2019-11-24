58e969534e97f0dcc75831e61dae5d916f3f92e2
tam-m <tamzhavanadze@gmail.com>
2019-11-24 12:29:20 +0400

    Add onclick event to SVG element

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,24 +1,23 @@
-// // let all_svg_buttons = document.getElementsByClassName('icon');
+let all_svg_buttons = document.getElementsByClassName('icon');
+let bl = all_svg_buttons.length;
 
+for (let i =0; i <bl; i++) {
+    all_svg_buttons [i].addEventListener('click', function(){
 
-// let bl = all_svg_buttons.length;
-
-// for (let i =0; i <bl; i++) {
-//     all_svg_buttons [i].addEventListener('click', function(){
-
-//         this.classList.toggle('active');
+        this.classList.toggle('active');
             
-//         if (this.classList.contains('active')) {
-//             this.style.background = '#fff';
-//             alert('jii');
-//         } else {    
-//             this.setAttributeNS(null,"background","green");
-
-//          } 
+        if (this.classList.contains('active')) {
+            this.style.background = 'red';
+        } else {    
+            this.style.background = '#fff';
+         } 
     
-//     })
-// }
+    })
+}
    
+
+
+
 const menuIcon = document.querySelector(".hamburger-menu");
 const navbar = document.querySelector(".navbar");
 
